34b2913c70e2fb3e18ee0c4a51bba733ac33ead2
Chau Tran <cctran96@gmail.com>
2021-05-16 18:18:15 -0700

    commit tree 6f83d6eea10aeb43acd69223fb0ebb303dc5c2e4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+    def new_array(array)
+        array[-1] = "and #{array[-1]}"
+        array
+    end
+    array.size > 2 ? new_array(array).join(', ') : array.join(' and ')
 end
